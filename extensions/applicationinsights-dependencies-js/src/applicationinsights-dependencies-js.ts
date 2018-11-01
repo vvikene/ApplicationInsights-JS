90f5da4983d9a5b1cb890ad8f23993fb7ff95ae8
Mark Wolff <marwolff@microsoft.com>
2018-11-01 10:21:07 -0700

    Add fetch monitoring to dependencies plugin

--- extensions/applicationinsights-dependencies-js/src/applicationinsights-dependencies-js.ts
+++ extensions/applicationinsights-dependencies-js/src/applicationinsights-dependencies-js.ts
@@ -1,6 +1,4 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { AjaxMonitor, IDependenciesPlugin } from "./ajax";
-
-export { AjaxMonitor as AjaxPlugin, IDependenciesPlugin };
+export { AjaxMonitor as AjaxPlugin, IDependenciesPlugin } from "./ajax";
